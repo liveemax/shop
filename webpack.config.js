90bbc720e68ee4864b6ffca79b12031d6788543b
liveemax <livemax.95@gmail.com>
2021-05-12 23:46:01 +0300

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,141 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const CopyPlugin = require("copy-webpack-plugin");
+const data=require('./src/data.json')
+const {page_meta,nav,breadcrumbs,stock,page_text}=data
+const {h1,title,meta_keywords,meta_description}=page_meta
+
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+const src=path.resolve(__dirname, 'src')
+const build=path.resolve(__dirname, 'dist')
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+
+    if (isProd) {
+        config.minimizer = [
+            new OptimizeCssAssetWebpackPlugin()
+        ]
+    }
+
+    return config
+}
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
+const cssLoaders = extra => {
+    const loaders = [
+        {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+                hmr: isDev,
+                reloadAll: true
+            },
+        },
+        'css-loader'
+    ]
+
+    if (extra) {
+        loaders.push(extra)
+    }
+
+    return loaders
+}
+
+const babelOptions = () => {
+    return {
+        presets: [
+            '@babel/preset-env'
+        ]
+    }
+}
+
+
+const jsLoaders = () => {
+    return [{
+        loader: 'babel-loader',
+        options: babelOptions()
+    }]
+}
+
+const plugins = () => {
+    return [
+        new HTMLWebpackPlugin({
+        templateParameters:{'h1': require('./src/data.json')["page_meta"]["h1"]||"TRUCKS"},
+        template: path.resolve(src,'index.html'),
+            minify:
+                {collapseWhitespace: isProd}
+}
+        ),
+        new CleanWebpackPlugin(),
+        new CopyPlugin([
+                {from: path.resolve(src, 'static'),
+                   to: path.resolve(build, 'static'),
+                    context: path.resolve("src", 'static')
+                },
+        ]),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        })
+    ]
+}
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: ['@babel/polyfill', path.resolve(src,'index.js')]
+    },
+    output: {
+        filename: filename('js'),
+        path: build
+    },
+    resolve: {
+        extensions: ['.js', '.json', '.png'],
+        alias: {
+            '@': src,
+        }
+    },
+    optimization: optimization(),
+    devServer: {
+        overlay: true,
+        open: true,
+        port: 82,
+        hot: isDev
+    },
+    devtool: isDev ? 'source-map' : '',
+    plugins: plugins(),
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: cssLoaders()
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders('sass-loader')
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: jsLoaders()
+            }
+        ]
+    }
+}
